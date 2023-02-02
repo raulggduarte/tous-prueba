57c8a654e8182dc82bda622c7abda93483fe5dc8
Raul Gallegos Duarte <raulgallegosduarte@MacBook-Air-de-Raul.local>
2023-02-02 14:59:42 +0100

    prueba-tous-dist

--- src/components/CtaVideo.vue
+++ src/components/CtaVideo.vue
@@ -1,7 +1,7 @@
 <template>
     <div v-on:click="playPause()">
-        <video-background class="video-background" :src="require(`@/assets/cta-video.mp4`)"
-            :poster="require(`@/assets/cta-video-pic.png`)" :autoplay=false style="max-height: 400px; height: 100vh;"
+        <video-background class="video-background" :src="require(`../assets/cta-video.mp4`)"
+            :poster="require(`../assets/cta-video-pic.png`)" :autoplay=false style="max-height: 400px; height: 100vh;"
             overlay="linear-gradient(45deg,#2a4ae430,#fb949e6b)" ref="videobackground">
             <img src="@/assets/play-triangle-outline.png"
             class="play-logo"
